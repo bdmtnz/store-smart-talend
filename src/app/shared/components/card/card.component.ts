073ac6722d0beb3cfc8423ab517ca8a96fb4d02e
bdmtnz <martinez_o@hotmail.es>
2024-04-19 19:47:31 -0500

    Advanced cards

--- src/app/shared/components/card/card.component.ts
+++ src/app/shared/components/card/card.component.ts
@@ -1,12 +1,25 @@
-import { Component } from '@angular/core';
+import { NgStyle } from '@angular/common';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-card',
   standalone: true,
-  imports: [],
+  imports: [NgStyle],
   templateUrl: './card.component.html',
   styleUrl: './card.component.scss'
 })
 export class CardComponent {
+  @Input({required: true}) _text!: string
+  @Input({required: true}) _icon!: string
+  @Input({required: false}) _size: number = 24
 
+  get text() {
+    return this._text
+  }
+  get icon() {
+    return this._icon
+  }
+  get size() {
+    return `${this._size}pt`
+  }
 }
--- src/app/shared/pages/start/start.component.ts
+++ src/app/shared/pages/start/start.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { CardComponent } from '../../components/card/card.component';
 
 @Component({
   selector: 'app-start',
   standalone: true,
-  imports: [],
+  imports: [CardComponent],
   templateUrl: './start.component.html',
   styleUrl: './start.component.scss'
 })
