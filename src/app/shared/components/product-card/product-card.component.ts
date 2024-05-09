7833e2cc429e611dda0417c949dec4fcbfeeecd6
bdmtnz <martinez_o@hotmail.es>
2024-05-09 00:41:42 -0500

    Advanced user start

--- src/app/shared/components/product-card/product-card.component.ts
+++ src/app/shared/components/product-card/product-card.component.ts
@@ -1,5 +1,5 @@
 import { NgStyle } from '@angular/common';
-import { AfterViewInit, Component, Input } from '@angular/core';
+import { AfterContentInit, AfterViewChecked, AfterViewInit, Component, DoCheck, Input, OnInit } from '@angular/core';
 import { Product } from '../../models/product';
 
 @Component({
@@ -11,12 +11,28 @@ import { Product } from '../../models/product';
   templateUrl: './product-card.component.html',
   styleUrl: './product-card.component.scss'
 })
-export class ProductCardComponent implements AfterViewInit {
-  ngAfterViewInit(): void {
-    this.getDominantColor(this._data.img)
-  }
+export class ProductCardComponent implements AfterViewChecked, AfterViewInit {
   @Input({required: true}) _data!: Product
+  //private backgroundColor: string = 'rgba(0, 0, 0, 0.022)'
+  private backgroundColor: string = 'transparent'
 
+  ngAfterViewChecked(): void {
+    //this.getDominantColor(this._data.img)
+  }
+
+  ngAfterViewInit(): void {
+    setTimeout(() => {
+      this.getDominantColor(this._data.img)
+    }, 10)
+  }
+
+  get bgColor() {
+    return this.backgroundColor
+  }
+
+  get canvaId() {
+    return `product-card-canva-${this._data.id}`
+  }
   get data() {
     return this._data
   }
@@ -33,17 +49,18 @@ export class ProductCardComponent implements AfterViewInit {
   getDominantColor(image: string) {
     const imageObject = new Image()
     imageObject.src = image
-    const canvas = document.getElementById("canvas") as HTMLCanvasElement
+    const canvas = document.getElementById(this.canvaId) as HTMLCanvasElement
     // preview = document.getElementById("preview"),
-    const ctx = canvas!.getContext("2d");
+    const ctx = canvas!.getContext("2d", { willReadFrequently: true });
     //draw the image to one pixel and let the browser find the dominant color
     ctx!.drawImage(imageObject, 0, 0, 1, 1);
   
     //get pixel color
     const i = ctx!.getImageData(0, 0, 1, 1).data;
   
-    console.log(`rgba(${i[0]},${i[1]},${i[2]},${i[3]})`);
-  
-    console.log("#" + ((1 << 24) + (i[0] << 16) + (i[1] << 8) + i[2]).toString(16).slice(1));
+    // this.backgroundColor = `rgba(${i[0]},${i[1]},${i[2]},${i[3]})`
+    this.backgroundColor = `rgba(${i[0]},${i[1]},${i[2]},0.1)`
+    // console.log(this.backgroundColor);
+    // console.log("#" + ((1 << 24) + (i[0] << 16) + (i[1] << 8) + i[2]).toString(16).slice(1));
   }
 }
